fix off-by-one in U-turn

python/adaptive_hmc.py
@@ -54,7 +54,7 @@ class AdaptiveHmcSampler:
         logp_tune_prop = self.logp_tune(theta_prop, rho_prop)
 
         self._proposed += 1
-        if np.log(self._rng.uniform()) < (logp_prop - logp) + (logp_tune_prop - logp_tune):
+        if np.log(self._rng.uniform()) < ((logp_prop + logp_tune_prop) - (logp + logp_tune)):
             self._accepted += 1
             self._theta = theta_prop
             self._rho = rho_prop

python/experiments.py
@@ -82,21 +82,26 @@ def uturn_normal(seed = 1234):
 def plot_normal(seed = 1234):
     D = 5
     model = models.StdNormal(D)
-    stepsize = 0.9
+    stepsize = 0.5
     M = 100_000
-    sampler = uts.UTurnSampler(model, stepsize, seed = seed)
+    sampler = uts.UTurnSampler(model, stepsize = stepsize, seed = seed)
     sample = sampler.sample(M)
+    print(f"E[X]: {np.mean(sample, axis=0)}")
+    print(f"E[X^2]: {np.mean(sample**2, axis=0)}")    
     df = pd.DataFrame({"x": sample[1:M, 1]})
+    # cf. fully random:
+    # df = pd.DataFrame({"x": np.random.randn(M)})
     plot = (
         pn.ggplot(df, pn.aes(x="x"))
         + pn.geom_histogram(
-            pn.aes(y="..density.."), bins=30, color="black", fill="white"
+            pn.aes(y="..density.."), bins=50, color="black", fill="white"
         )
         + pn.stat_function(
             fun=sp.stats.norm.pdf, args={"loc": 0, "scale": 1}, color="red", size=1
         )
     )
     print(plot)
+
 
 import u_turn_sampler as uts
 import models
@@ -159,12 +164,12 @@ def test_model(config, M, seed = None):
 
 s = 983459874
 M = 500 * 500
-etest_model(std_normal, M = M, seed = s) 
-test_model(eight_schools, M = M, seed = s)
+# test_model(std_normal, M = M, seed = s) 
+# test_model(eight_schools, M = M, seed = s)
     
 
 # stan_model_experiment()    
-stan_model_experiment_b()    
-# plot_normal()
+# stan_model_experiment_b()    
+plot_normal(647483)
 # uturn_normal(seed = 67375765)    
-uturn_eight_schools(seed = 67375765)    
+# uturn_eight_schools(seed = 67375765)    

python/reference.py
@@ -56,7 +56,7 @@ def test_model(config, M, seed = None):
     print(f"{means_sq = }")
     print(f"metric: {np.mean(fit.metric, axis = 0)}")
     model2 = models.StanModel(model_path, data = data_path, seed = seed)
-    stepsize = 0.5
+    stepsize = 0.9
     seed = 12345
     with open(data_path, 'r') as f:
         data_dict = json.load(f)

python/u_turn_sampler.py
@@ -11,52 +11,29 @@ class UTurnSampler(ahmc.AdaptiveHmcSampler):
         super().__init__(model, stepsize, numsteps, seed, theta0, rho0)
         self._gradient_calls = 0
         self._leapfrog_steps = 0
-        self._uniform_steps = True
-
-    def uturn_to_steps(self, N):
-        return N + 1
 
     def uturn(self, theta, rho):
         theta_next = theta
         rho_next = rho
         last_dist = 0
-        L = 0
+        N = 0
         while True:
-            L += 1
             theta_next, rho_next = self.leapfrog_step(theta_next, rho_next)
             dist = np.sum((theta_next - theta)**2) # sp.spatial.distance.euclidean(theta_next, theta)
             if dist <= last_dist:
-                return L
+                return N
             last_dist = dist
+            N += 1
 
     def sample_tuning(self):
         N = self.uturn(self._theta, self._rho)
-        steps = self.uturn_to_steps(N)
-
-        if self._uniform_steps:
-            self._numsteps = self._rng.integers(1, steps)
-        else: 
-            p = np.arange(1, steps + 1)
-            p = p / np.sum(p)
-            self._numsteps = np.random.choice(a = np.arange(1, steps + 1), p = p)
-
-        if self._numsteps <= N:
-            self._gradient_calls -= self._numsteps  # adjustment for overlap
-        else:
-            self._gradient_calls += self._numsteps  # rev subseq of fwd
+        self._numsteps = self._rng.integers(1, N + 1)
+        self._gradient_calls -= self._numsteps  # adjustment for overlap
         self._leapfrog_steps += self._numsteps
 
     def logp_tune(self, theta, rho):
         N = self.uturn(theta, rho)
-        if self._numsteps > self.uturn_to_steps(N):
-            return np.log(0) # U-turn before return
-        if self._numsteps <= N:  # add forward and reverse
-            self._gradient_calls += N
-        steps = self.uturn_to_steps(N)
-
-        if self._uniform_steps:
-            return -np.log(self.uturn_to_steps(N) - 1)  # steps selected exclusive of end
-        else:
-            p = np.arange(1, steps + 1)**0.5
-            p = p / np.sum(p)
-            return np.log(p[self._numsteps - 1])
+        # if self._numsteps > self.uturn_to_steps(N):
+        #    return np.log(0) # U-turn before return
+        self._gradient_calls += N
+        return -np.log(N)