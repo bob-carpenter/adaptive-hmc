040d7738eeca52b367b781fb6f9ef17a7ab1d4a5
Bob Carpenter <bcarpenter@flatironinstitute.org>
2024-03-20 12:01:19 -0400

    debugged turnaround

--- python/turnaround_two.py
+++ python/turnaround_two.py
@@ -10,6 +10,9 @@ class TurnaroundSampler:
         self._rng = rng
         self._theta = self._rng.normal(size=model.param_unc_num())
         self._rho = self._rng.normal(size=model.param_unc_num())
+        self._too_short_rejects = 0
+        self._fwds = []
+        self._bks = []
 
     def __iter__(self):
         return self
@@ -34,7 +37,7 @@ class TurnaroundSampler:
         return self._model.log_density(theta) - 0.5 * sum(rho**2)
 
     def uturn(self, theta, rho):
-        return self.uturn_angle(theta, rho)
+        return self.uturn_distance(theta, rho)
     
     def uturn_angle(self, theta, rho):
         theta_next = theta
@@ -61,21 +64,26 @@ class TurnaroundSampler:
     
     def draw(self):
         self._rho = self._rng.normal(size=self._model.param_unc_num())
-        logp = self.log_joint(self._theta, self._rho)
-        N = self.uturn(self._theta, self._rho)
-        L = self._rng.integers(0, N)
-        theta_prop, rho_prop = self.leapfrog(self._theta, self._rho, L)
-        rho_prop = -rho_prop
-        logp_prop = self.log_joint(theta_prop, rho_prop)
-        M = self.uturn(theta_prop, rho_prop)
-        if (M < L):
-            print("unbalanced")
-            return self._theta, self._rho  # cannot balance
-        logp_fwd = -np.log(N)
-        logp_bk = -np.log(M)
-        if np.log(self._rng.uniform()) < (logp_prop - logp) + (logp_bk - logp_fwd):
-            self._theta = theta_prop
-            self._rho = rho_prop
+        theta0 = self._theta
+        rho0 = self._rho
+        L = self.uturn(theta0, rho0)
+        N1 = self._rng.integers(1, L)
+        theta1_star, rho1_star = self.leapfrog(self._theta, self._rho, N1) # F^(N1)(theta0, rho0)
+        rho1_star = -rho1_star # S.F^(N1)(theta0, rho0)
+
+        Lstar = self.uturn(theta1_star, rho1_star)
+        self._fwds.append(L)
+        self._bks.append(Lstar)
+        if Lstar - 1 < N1:
+            self._too_short_rejects += 1
+            return self._theta, self._rho  # unbalance-able
+        
+        log_alpha = ( (self.log_joint(theta1_star,rho1_star) + -np.log(Lstar - 1))
+                          - (self.log_joint(theta0, rho0) + -np.log(L - 1) ) )
+        
+        if np.log(self._rng.uniform()) < log_alpha:
+            self._theta = theta1_star
+            self._rho = rho1_star
         return self._theta, self._rho
 
     def sample(self, M):
