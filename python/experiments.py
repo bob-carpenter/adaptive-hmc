58c1114f0829e24dfc0a78d87444cd7bb1ee4645
Bob Carpenter <carp@alias-i.com>
2024-03-23 15:56:43 -0400

    test harness

--- python/experiments.py
+++ python/experiments.py
@@ -1,8 +1,12 @@
 import turnaround as ta
+import cmdstanpy as csp
 import numpy as np
 import bridgestan as bs
 import plotnine as pn
 import pandas as pd
+import warnings
+warnings.filterwarnings("ignore", message="Loading a shared object .* that has already been loaded.*")
+
 
 def sq_jumps(draws):
     M = np.shape(draws)[0]
@@ -65,9 +69,17 @@ def constrain(model, draws):
         draws_constr[m, :] = model.param_constrain(draws[m, :])
     return draws_constr
 
-def turnaround_experiment(model, stepsize, num_draws, seed):
+def turnaround_experiment(program_path, data, stepsize, num_draws,
+                              uturn_condition, path_fraction, seed):
+    print(f"CONFIG: {program_path=}; {stepsize=};  {num_draws=}; {uturn_condition=}; {path_fraction=}; {seed=}")
+    model_bs = bs.StanModel(model_lib=program_path, data=data,
+                         capture_stan_prints=False)
+    # model_csp = csp.CmdStanModel(stan_file=program_path)
     rng = np.random.default_rng(seed)
-    sampler = ta.TurnaroundSampler(model=model, stepsize=stepsize, rng=rng)
+    sampler = ta.TurnaroundSampler(model=model_bs, stepsize=stepsize,
+                              rng=rng,
+                              uturn_condition=uturn_condition,
+                              path_fraction=path_fraction)
     constrained_draws = sampler.sample_constrained(num_draws)
     # print(f"Mean(param): {np.mean(constrained_draws, axis=0)}")
     # print(f"Mean(param^2): {np.mean(constrained_draws**2, axis=0)}")
@@ -77,22 +89,27 @@ def turnaround_experiment(model, stepsize, num_draws, seed):
     print(f"Mean square jump distance: {np.mean(sq_jumps(constrained_draws)):5.1f}")
     rejects, prop_rejects = num_rejects(constrained_draws)
     print(f"Proportion rejects: {prop_rejects:5.3f}")
-    print(f"Cannot get back rejects: {sampler._cannot_get_back_rejects / num_draws}")
+    print(f"Cannot return rejects: {sampler._cannot_get_back_rejects / num_draws}")
     # print("(Forward steps to U-turn from initial, Backward steps to U-turn from proposal)")
     # for n in range(10):
     #   print(f"  ({sampler._fwds[n]:3d},  {sampler._bks[n]:3d})")
 
 
-model = bs.StanModel(model_lib='../stan/normal.stan', data='{"D": 100}',
-                         capture_stan_prints=False)
-for step in [0.8, 0.4, 0.2, 0.1, 0.05]:
-    print(f"\n{step = }")
-    turnaround_experiment(model = model, stepsize=step, num_draws = 10_000,
-        seed=997459)    
+normal = ('../stan/normal.stan', '{"D": 100}')
+eight_schools = ('../stan/eight-schools.stan', '../stan/eight-schools.json')
+num_draws = 1_000
 
-    # turnaround_experiment('../stan/eight-schools.stan', data='../stan/eight-schools.json',
-    # stepsize=step, num_draws = 10_000,
-    # seed=997459)
 
+
+for program_path, data in [eight_schools, normal]:
+       for step in [0.8, 0.4, 0.2, 0.1, 0.05]:
+           print("")
+           turnaround_experiment(program_path = program_path,
+                              data = data,
+                              stepsize=step,
+                              num_draws = num_draws,
+                              uturn_condition = 'distance',
+                              path_fraction='full',
+                              seed=997459)    
     
 
