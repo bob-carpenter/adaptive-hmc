fix sign in step accept

python/experiments.py
@@ -76,23 +76,24 @@ def constrain(model, draws):
 def metadata_columns(fit):
     return len(fit.metadata.method_vars.keys())
 
+def dict_draw(thetas_dict, n):
+    return {name:draws[n] for name, draws in thetas_dict.items()}
+
 def nuts_adapt(program_path, data_path, seed):
     model = csp.CmdStanModel(stan_file = program_path)
     fit = model.sample(data = data_path, seed=seed,
                            metric="unit_e", show_console=False,
                            # adapt_delta=0.95,
-                           chains=1, iter_warmup=2_000, iter_sampling=10_000,
+                           chains=1, iter_warmup=5_000, iter_sampling=40_000,
                            show_progress=False)
     thetas_dict = fit.stan_variables()
-    theta_draw_dict = {name:draws[0] for name, draws in thetas_dict.items()}
     N = metadata_columns(fit)
     theta_draws = fit.draws(concat_chains=True)[:, N:]
-    theta_draw_array = theta_draws[5, :]  # number 5 is arbitrary
     theta_hat = theta_draws.mean(axis=0)
     theta_sq_hat = (theta_draws**2).mean(axis=0)
     metric = fit.metric
     step_size = fit.step_size
-    return theta_draw_dict, theta_draw_array, theta_hat, theta_sq_hat, metric, step_size
+    return thetas_dict, theta_draws, theta_hat, theta_sq_hat, metric, step_size
 
     
 def nuts(program_path, data_path, inits, step_size, draws, seed):
@@ -127,15 +128,13 @@ def nuts_experiment(program_path, data, inits, seed, theta_hat, theta_sq_hat, dr
     
 
 def turnaround_experiment(program_path, data, theta_unc, stepsize, num_draws,
-                              uturn_condition, path_frac, theta_hat, theta_sq_hat,
-                              seed):
+        path_frac, theta_hat, theta_sq_hat, seed):
     model_bs = bs.StanModel(model_lib=program_path, data=data,
                          capture_stan_prints=False)
     rng = np.random.default_rng(seed)
     theta = model_bs.param_unconstrain(theta_unc)
     sampler = ta.TurnaroundSampler(model=model_bs, stepsize=stepsize,
                                        theta=theta, rng=rng,
-                                       uturn_condition=uturn_condition,
                                        path_frac=path_frac)
     constrained_draws = sampler.sample_constrained(num_draws)
     rejects, prop_rejects = num_rejects(constrained_draws)
@@ -146,7 +145,8 @@ def turnaround_experiment(program_path, data, theta_unc, stepsize, num_draws,
     theta_sq_hat_turnaround = (constrained_draws**2).mean(axis=0)
     rmse = root_mean_square_error(theta_hat, theta_hat_turnaround)
     rmse_sq = root_mean_square_error(theta_sq_hat, theta_sq_hat_turnaround)
-    print(f"AHMC({uturn_condition}, {path_frac}): MSJD={msjd:8.3f};  leapfrog_steps={sampler._gradient_evals}  reject={prop_rejects:4.2f};  no return={prop_no_return:4.2f};  diverge={prop_diverge:4.2f};  RMSE(theta)={rmse:8.4f};  RMSE(theta**2)={rmse_sq:8.4f}")
+    print(f"AHMC({path_frac}): MSJD={msjd:8.3f};  leapfrog_steps={sampler._gradient_evals}  reject={prop_rejects:4.2f};  no return={prop_no_return:4.2f};  diverge={prop_diverge:4.2f};  RMSE(theta)={rmse:8.4f};  RMSE(theta**2)={rmse_sq:8.4f}")
+    return "AHMC", path_frac, stepsize, sampler._gradient_evals, prop_rejects, prop_no_return, rmse, rmse_sq, msjd
     # print(f"Mean(param): {np.mean(constrained_draws, axis=0)}")
     # print(f"Mean(param^2): {np.mean(constrained_draws**2, axis=0)}")
     # scalar_draws_for_traceplot = constrained_draws[: , 0]
@@ -157,56 +157,189 @@ def turnaround_experiment(program_path, data, theta_unc, stepsize, num_draws,
     #   print(f"  ({sampler._fwds[n]:3d},  {sampler._bks[n]:3d})")
 
 
+def model_steps():
+    normal = ('normal', [0.5, 0.25])# for 500: [0.36, 0.18])
+    corr_normal = ('correlated-normal', [0.12, 0.06])
+    ill_normal = ('ill-condition-normal', [0.1, 0.05])
+    eight_schools = ('eight-schools', [0.5, 0.25])
+    irt = ('irt-2pl', [0.05, 0.025])
+    lotka_volterra = ('lotka-volterra', [0.018, 0.009])
+    arK = ('arK', [0.01, 0.005])
+    garch = ('garch', [0.16, 0.08])
+    normal_mix = ('normal-mixture', [0.01, 0.005])
+    hmm = ('hmm', [0.025, 0.0125])
+    pkpd = ('pkpd', [0.1, 0.05])
+    arma = ('arma', [0.016, 0.008])
+    poisson_glmm = ('glmm-poisson', [0.008, 0.004])
+    covid = ('covid19-imperial-v2', [0.01])
+    prophet = ('prophet', [0.0006, 0.0003])
+    return [normal , ill_normal, corr_normal, irt, poisson_glmm, eight_schools, normal_mix, hmm, arma, garch, arK, pkpd, lotka_volterra, prophet] # [covid]
 
-normal = ('normal', [0.36, 0.18])
-corr_normal = ('correlated-normal', [0.12, 0.06])
-eight_schools = ('eight-schools', [0.5, 0.25])
-irt = ('irt-2pl', [0.05, 0.025])
-lotka_volterra = ('lotka-volterra', [0.018, 0.009])
-arK = ('arK', [0.01, 0.005])
-garch = ('garch', [0.16, 0.08])
-normal_mix = ('normal-mixture', [0.01, 0.005])
-hmm = ('hmm', [0.025, 0.0125])
-pkpd = ('pkpd', [0.1, 0.05])
-arma = ('arma', [0.016, 0.008])
-poisson_glmm = ('glmm-poisson', [0.008, 0.004])
-covid = ('covid19-imperial-v2', [0.01])
-prophet = ('prophet', [0.0006, 0.0003])
+def all_vs_nuts():
+    stop_griping()
+    meta_seed = 57484894
+    seed_rng = np.random.default_rng(meta_seed)
+    seeds = seed_rng.integers(low=0, high=2**32, size=2)
+    num_draws = 100
+    print(f"NUM DRAWS: {num_draws}  SEEDS: {seeds}")
+    for program_name, step_sizes in model_steps():
+        program_path = '../stan/' + program_name + '.stan'
+        data_path = '../stan/' + program_name + '.json'
+        print(f"\nMODEL: {program_path}")
+        print("============================================================")
+        nuts_draws_dict, nuts_draws_array, theta_hat, theta_sq_hat, adapted_metric, adapted_step_size = nuts_adapt(program_path=program_path, data_path=data_path, seed=seeds[0])
+        num_unc_params = np.shape(nuts_draws_array[1, :])[0]
+        print(f"# unconstrained parameters = {num_unc_params}")
+        print(f"NUTS: adapted step size = {adapted_step_size}")
+        for step_size in step_sizes:
+            print(f"\nSTEP SIZE = {step_size}")
+            for m, seed in enumerate(seeds):
+                DRAW_INDEX = 5  # chosen arbitrarily
+                nuts_draw_dict =  dict_draw(nuts_draws_dict, DRAW_INDEX + 10 * m)
+                nuts_draw_array = nuts_draws_array[DRAW_INDEX + 10 * m, :]
+                nuts_experiment(program_path=program_path, data=data_path,
+                                    inits=nuts_draw_dict, step_size=step_size, theta_hat=theta_hat,
+                                    theta_sq_hat=theta_sq_hat, draws=num_draws, seed=seed)
+                for uturn_condition in ['distance']:  # 'sym_distance'
+                    for path_frac in [0.5, 0.6, 0.7, 0.8]:  # ['full', 'half', 'quarter'] for uniform
+                        turnaround_experiment(program_path=program_path,
+                                                data=data_path,
+                                                theta_unc=np.array(nuts_draw_array),
+                                                stepsize=step_size,
+                                                num_draws=num_draws,
+                                                path_frac=path_frac,
+                                                theta_hat=theta_hat,
+                                                theta_sq_hat=theta_sq_hat,
+                                                seed=seed)
 
-model_steps = [normal, corr_normal, irt, poisson_glmm, eight_schools,
-                   normal_mix, hmm, arma, garch, arK, pkpd, lotka_volterra, prophet] # [covid]
 
-stop_griping()
-meta_seed = 57484894
-seed_rng = np.random.default_rng(meta_seed)
-seeds = seed_rng.integers(low=0, high=2**32, size=2)
-print(f"SEEDS: {seeds}")
-num_draws = 400
-for program_name, step_sizes in model_steps:
+
+def binomial_prob_plot():
+    stop_griping()
+    num_seeds = 100
+    num_draws = 100
+    meta_seed = 57484894
+    seed_rng = np.random.default_rng(meta_seed)
+    seeds = seed_rng.integers(low=0, high=2**32, size=num_seeds)
+    print(f"NUM DRAWS: {num_draws}  SEEDS: {seeds}")
+    program_name, step_sizes = 'normal', [0.5, 0.25]  # for 500 dims: [0.36, 0.18])
     program_path = '../stan/' + program_name + '.stan'
     data_path = '../stan/' + program_name + '.json'
-    print(f"\nMODEL: {program_path}")
-    print("============================================================")
-    nuts_draw_dict, nuts_draw_array, theta_hat, theta_sq_hat, adapted_metric, adapted_step_size = nuts_adapt(program_path=program_path, data_path=data_path, seed=seeds[0])
-    print(f"# unconstrained parameters = {np.shape(nuts_draw_array)[0]}")
-    print(f"NUTS: adapted step size = {adapted_step_size}")
+    nuts_draws_dict, nuts_draws_array, theta_hat, theta_sq_hat, adapted_metric, adapted_step_size = nuts_adapt(program_path=program_path, data_path=data_path, seed=seeds[0])
+    columns = ['stepsize', 'binom_prob', 'val_type', 'val']   # val_type in ['steps', 'reject', 'no_return', 'rmse', 'rmse_sq']
+    df = pd.DataFrame(columns=columns)
     for step_size in step_sizes:
-        print(f"\nSTEP SIZE = {step_size}")
-        for seed in seeds:
-            nuts_experiment(program_path=program_path, data=data_path,
-                                inits=nuts_draw_dict, step_size=step_size, theta_hat=theta_hat,
-                                theta_sq_hat=theta_sq_hat, draws=num_draws, seed=seed)
-            for uturn_condition in ['distance']:  # 'sym_distance'
-                for path_frac in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:  # ['full', 'half', 'quarter'] for uniform
-                    turnaround_experiment(program_path=program_path,
-                                            data=data_path,
-                                            theta_unc=np.array(nuts_draw_array),
-                                            stepsize=step_size,
-                                            num_draws=num_draws,
-                                            uturn_condition=uturn_condition,
-                                            path_frac=path_frac,
-                                            theta_hat=theta_hat,
-                                            theta_sq_hat=theta_sq_hat,
-                                            seed=seed)
-    
+        print(f"STEP SIZE: {step_size}")
+        for m, seed in enumerate(seeds):
+            print(f"{m=}  {seed=}")
+            idx = 100 * m
+            nuts_draw_dict =  dict_draw(nuts_draws_dict, idx)
+            nuts_draw_array = nuts_draws_array[idx, :]
+            for binom_prob in [0.1, 0.2, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.9]:
+                sampler, binom_prob, stepsize, steps, reject, no_return, rmse, rmse_sq, msjd = turnaround_experiment(program_path=program_path,
+                                                                                                                         data=data_path,
+                                                                                                                         theta_unc=np.array(nuts_draw_array),
+                                                                                                                         stepsize=step_size,
+                                                                                                                         num_draws=num_draws,
+                                                                                                                         path_frac=binom_prob,
+                                                                                                                         theta_hat=theta_hat,
+                                                                                                                         theta_sq_hat=theta_sq_hat,
+                                                                                                                         seed=seed)
 
+                df.loc[len(df)] = stepsize, binom_prob, 'Leapfrog Steps', steps
+                df.loc[len(df)] = stepsize, binom_prob, 'Reject', reject
+                df.loc[len(df)] = stepsize, binom_prob, 'No Return', no_return
+                df.loc[len(df)] = stepsize, binom_prob, 'RMSE (param)', rmse
+                df.loc[len(df)] = stepsize, binom_prob, 'RMSE (param sq)', rmse_sq
+                df.loc[len(df)] = stepsize, binom_prob, 'MSJD', msjd
+
+
+
+                # WORKS FOR SINGLE SEED            
+                # plot = (pn.ggplot(df, pn.aes(x='binom_prob', y='val', group='stepsize', color='factor(stepsize)'))
+                #            + pn.geom_line()
+                #            + pn.scale_x_continuous(limits=(0, 1), breaks = [0, 0.25, 0.5, 0.75, 1], labels=["0", "1/4", "1/2", "3/4", "1"])
+                #            + pn.coord_fixed(ratio=1)
+                #            + pn.labs(y = '', x='binomial success probability', color="step size")
+                #            + pn.facet_wrap('~ val_type', scales='free_y', ncol=5))
+                # plot.show()
+
+                agg_df = df.groupby(['stepsize', 'val_type', 'binom_prob']).agg(
+                    mean_val=('val', 'mean'),
+                    #    std_val=('val', 'std'),
+                    lower_quantile=('val', lambda x: x.quantile(0.1)),  # For 80% CI, lower bound
+                    upper_quantile=('val', lambda x: x.quantile(0.9))   # For 80% CI, upper bound
+                    ).reset_index()
+
+                plot = (pn.ggplot(agg_df, pn.aes(x='binom_prob', y='mean_val', ymin='lower_quantile', ymax='upper_quantile', group='stepsize', color='factor(stepsize)'))
+                            + pn.geom_line(size=0.5)
+                            + pn.scale_x_continuous(limits=(0, 1), breaks = [0, 0.25, 0.5, 0.75, 1], labels=["0", "1/4", "1/2", "3/4", "1"])
+                            + pn.coord_fixed(ratio=1)
+                            + pn.labs(y = '', x='Binomial Success Probability', color="Step Size")
+                            + pn.facet_wrap('~ val_type', scales='free_y', ncol=3))
+                plot.save(filename='binomial_prob_steps_plot.pdf', width=8.5, height=5)
+                # plot.show()
+
+all_vs_nuts()
+
+seed = 987599123
+stepsize = 0.25
+D = 100
+program_name = 'normal'
+program_path = '../stan/' + program_name + '.stan'
+data_path = '../stan/' + program_name + '.json'
+model_bs = bs.StanModel(model_lib=program_path, data=data_path,
+                        capture_stan_prints=False)
+rng = np.random.default_rng(seed)
+theta0 = rng.normal(loc=0, scale=1, size=D)
+sampler = ta.TurnaroundSampler(model=model_bs, stepsize=stepsize,
+                               theta=theta0,
+                               path_frac=0.6, rng=rng)
+N = 100_000
+
+draws = sampler.sample_constrained(N)
+# draws = rng.normal(size=(N, D))  # use for validating plotting code
+
+cumsum_draws = np.cumsum(draws, axis=0)
+divisors = np.arange(1, draws.shape[0] + 1).reshape(-1, 1)
+abs_err = np.abs(cumsum_draws) / divisors
+avg_abs_err = np.mean(abs_err, axis=1)
+
+# for square draws, expected value is 1
+draws_sq = draws**2
+cumsum_draws_sq = np.cumsum(draws_sq, axis=0)
+# catastrophic cancel when result close to 1?
+abs_err_sq = np.abs(cumsum_draws_sq / divisors - 1)  # expected value is E[ChiSquare(1)] = 1
+avg_abs_err_sq = np.mean(abs_err_sq, axis=1)
+
+errs = np.concatenate([avg_abs_err, avg_abs_err_sq])
+estimands = np.concatenate([np.array(['theta'] * N),
+                                   np.array(['theta**2'] * N)])
+
+iteration = np.arange(1, len(avg_abs_err) + 1)
+iterations = np.concatenate([iteration, iteration])
+df = pd.DataFrame({
+    'iteration': iterations,
+    'E[|err|]': errs,
+    'estimand': estimands
+})
+
+lines_df = pd.DataFrame({
+    'estimand': np.array(['theta', 'theta**2']),
+    'x': np.array([10, 10]),
+    'y': np.array([1 / np.sqrt(10), np.sqrt(2) / np.sqrt(10)]),
+    'xend': np.array([N, N]),
+    'yend': np.array([1 / np.sqrt(N), np.sqrt(2) / np.sqrt(N)])
+})
+
+plot = (pn.ggplot(df, pn.aes(x='iteration', y='E[|err|]'))
+            + pn.geom_line()
+            + pn.scale_x_log10(limits=(10,N)) # breaks=[10**0, 10**1, 10**2, 10**3, 10**4], limits=(10**0, 10**4))
+            + pn.scale_y_log10() # breaks=[10**-6, 10**-5, 10**-4, 10**-3, 10**-2, 10**-1, 10**0], limits=(10**-6, 10**0))
+            + pn.geom_segment(data=lines_df,
+                     mapping=pn.aes(x='x', y='y', xend='xend', yend='yend'),
+                     linetype='dotted')
+            + pn.facet_wrap('~ estimand')
+)
+plot.save(filename='learning_curve.pdf', width=8, height=4)
+# plot.show()

python/turnaround_binomial.py
@@ -10,7 +10,7 @@ class TurnaroundSampler(hmc.HmcSamplerBase):
     proportional to number of steps, flips momentum, then balances
     with reverse proposal probability. 
     """
-    def __init__(self, model, stepsize, theta, rng, uturn_condition, path_frac=0.5,
+    def __init__(self, model, stepsize, theta, rng, path_frac=0.5,
                      max_leapfrog = 1024):
         super().__init__(model, stepsize, rng)
         self._max_leapfrog_steps = max_leapfrog
@@ -50,7 +50,6 @@ class TurnaroundSampler(hmc.HmcSamplerBase):
         return lengths, p
 
     def sample_length(self, L):
-        lengths, p = self.range_probs(L)
         n = self._rng.binomial(L, self._success_prob)
         return n
 
@@ -84,9 +83,9 @@ class TurnaroundSampler(hmc.HmcSamplerBase):
                 self._cannot_get_back_rejects += 1   # DIAGNOSTIC
                 return self._theta, self._rho        # cannot balance w/o return
             log_accept = ((self.log_joint(theta_star, rho_star)
-                               - self.length_log_prob(N, Lstar))
+                               + self.length_log_prob(N, Lstar))
                           - (log_joint_theta_rho
-                                 - self.length_log_prob(N, L)))
+                                + self.length_log_prob(N, L)))
             if np.log(self._rng.uniform()) < log_accept:
                 self._theta = theta_star
                 self._rho = rho_star